54083e16c3298f39367931aa25bdf18a2fca0f21
Shivansh Srivastava <shivansh.srivastava0403@gmail.com>
2024-03-11 23:36:12 +0530

    Change in multer middleware setup

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -2,10 +2,9 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp")
+        cb(null, "./uploads")
     },
     filename: function (req, file, cb) {
-
         cb(null, file.originalname)
     }
 })
